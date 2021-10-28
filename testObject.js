a639810b42f3e668fafded468f801396cb4be6ea
StokicDusan <stokicdusan@gmail.com>
2021-10-28 11:39:46 +0200

    add an object for testing

---
+++ testObject.js
@@ -0,0 +1,10 @@
+const people = [
+    { name: 'name1', age: 21, position: 'position1' },
+    { name: 'name2', age: 31, position: 'position2' },
+    { name: 'name3', age: 23, position: 'position3' },
+    { name: 'name4', age: 19, position: 'position4' },
+    { name: 'name5', age: 19, position: 'position4' },
+    { name: 'name6', age: 22, position: 'position4' },
+    { name: 'name7', age: 31, position: 'position3' },
+    { name: 'name8', age: 19, position: 'position1' },
+];
