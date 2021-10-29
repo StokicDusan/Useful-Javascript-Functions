6e385934813f70b6f5ad4098c94005fb67fac40d
StokicDusan <stokicdusan@gmail.com>
2021-10-29 21:56:02 +0200

    add more test objects

---
+++ scripts/objectsForTesting.js
@@ -0,0 +1,24 @@
+const people = [
+    { name: 'name1', age: 21, position: 'position1', salary: 100 },
+    { name: 'name2', age: 31, position: 'position2', salary: 500 },
+    { name: 'name3', age: 23, position: 'position3', salary: 230 },
+    { name: 'name4', age: 19, position: 'position4', salary: 350 },
+    { name: 'name5', age: 19, position: 'position4', salary: 350 },
+    { name: 'name6', age: 22, position: 'position4', salary: 350 },
+    { name: 'name7', age: 31, position: 'position3', salary: 220 },
+    { name: 'name8', age: 19, position: 'position1', salary: 100 },
+];
+
+const fruit = ['orange', 'banana', 'banana', 'lemon', 'apple'];
+
+const friends = ['josh', 'peter', 'bob', 'anna', 'kelly'];
+
+const person = {
+    first: 'Aron',
+    last: 'Gunnarsson',
+    city: 'Akureyri',
+    job: 'Football player',
+    siblings: {
+        brother: 'Arnor Thor',
+    },
+};
