0e963243af0503b1c7a9a12a9ae8b44fa3a4d5fb
StokicDusan <stokicdusan@gmail.com>
2021-11-02 15:07:59 +0100

    add comments for logs

--- scripts/dynamicObjectKeys.js
+++ scripts/dynamicObjectKeys.js
@@ -1,13 +1,12 @@
 //dynamically change keys in the object using square bracket notation
 
-
 // dot notation
 const person = {
-    name: 'john',
+    name: 'steve',
 };
-console.log(person.name);
+console.log(person.name);                   // steve
 person.age = 24;
-console.log(person);
+console.log(person);                        // { name: 'steve', age: 24 }
 
 // square bracket notation
 
@@ -15,8 +14,8 @@ const items = {
     'featured-items': ['item1', 'item2']
 };
 
-console.log(items["featured-items"]);
-console.log(person["name"]);
+console.log(items["featured-items"]);       // [ 'item1', 'item2' ]
+console.log(person["name"]);                // steve
 
 let appState = "loading";
 const keyName = "computer";
@@ -31,12 +30,11 @@ const app2 = {
     [appState]: true
 }
 
-console.log(app1, app2);
+console.log(app1, app2);                    // { loading: true } { error: true }
 
 app1[keyName] = "apple";
 
-console.log(app1, app2);
-
+console.log(app1, app2);                    // { loading: true, computer: 'apple' } { error: true }
 
 //example where this is useful
 
@@ -50,12 +48,12 @@ function updateState(key, value) {
     state[key] = value
 }
 
-console.log(state);
+console.log(state);                         // { loading: true, computer: 'apple' } { error: true }
 updateState("name", "dusan");
-console.log(state);
+console.log(state);                         // { loading: true, name: 'dusan', job: '' }
 updateState("job", "engineer");
-console.log(state);
+console.log(state);                         // { loading: true, name: 'dusan', job: 'engineer' }
 updateState("product", "");
-console.log(state);
+console.log(state);                         // { loading: true, name: 'dusan', job: 'engineer', product: '' }
 updateState("product", "vegan food truck");
-console.log(state);
+console.log(state);                         // { loading: true, name: 'dusan', job: 'engineer', product: 'vegan food truck' }
