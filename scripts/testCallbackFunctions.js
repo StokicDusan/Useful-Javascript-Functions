d979ab61d3f6963f42cb731af72d3473c7b5ebb3
StokicDusan <stokicdusan@gmail.com>
2021-11-08 13:22:14 +0100

    add callback function script

---
+++ scripts/testCallbackFunctions.js
@@ -0,0 +1,34 @@
+// callback function is a function that we pass in as an argument
+// and execute it later
+
+function makeUppercase(value){
+    console.log(value.toUpperCase())
+}
+// makeUppercase('dusan');
+
+function reverseString(value){
+    console.log(value.split('').reverse().join(''))
+}
+
+function handleName(name,cb){
+    const fullName = `${name} stokic`
+    cb(fullName)
+    //some more logic
+    cb(fullName)
+    cb(fullName)
+}
+
+handleName('dusan',makeUppercase); // we are not invoking but referencing makeUppercase function
+handleName('dusan',reverseString);
+handleName('susan',function(value){
+    console.log(value)
+})
+handleName('susan', (value) => console.log(value))
+
+// array methods, setTimeout, event listeners etc
+
+const btn = document.querySelector("#btn")
+
+btn.addEventListener('click',()=>{
+    console.log('hello world')
+})
