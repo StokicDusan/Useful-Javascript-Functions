41550319b8005a1ba827cb5ec953317df037e66d
StokicDusan <stokicdusan@gmail.com>
2021-11-09 07:56:18 +0100

    add promise section

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ Table of contents:
     - [Spread Operator](#spread-operator)
     - [Array from()](#array-from)
     - [Optional Chaining ?.](#optional-chaining-)
+    - [Promises](#promises)
   - [Provide Feedback](#provide-feedback)
 
 ## How to use it
@@ -347,6 +348,26 @@ people.forEach((item) => {
 ```
 Iterating over the list in the solution with optional chaining gives the same result as the one without except that it can give an answer for a non-existant property instead of *undefined*.  
 The optional chaining solution is also shorter and simpler to implement.
+
+### Promises
+
+A JavaScript Promise object contains both the producing code and calls to the consuming code. When the executing code obtains the result, it should call one of the two callbacks, Success or Error.  
+A JavaScript Promise object can be: Pending, Fulfilled, Rejected.
+
+```js
+const value = 2
+const promise = new Promise((resolve,reject)=>{
+    const random = Math.floor(Math.random() * 3)
+    if(random === value){
+        resolve('hello world')
+    }else{
+        reject('there was an error')
+    }
+})
+
+console.log(promise)                // Promise {}
+promise.then((data)=>console.log(data)).catch((err)=>console.log(err));
+```
 
 
 ## Provide Feedback
---
+++ scripts/testPromisesExample.js
@@ -0,0 +1,57 @@
+// Promises examples
+// resolving callback hell from testCallbackFunctionsHell.js
+// coloring second and third referencting the first
+
+// #frist - after 1s
+// #second - in 3s after first
+// #third - in 2s after second
+// IN SEQUENCE !!!
+
+const btn = document.querySelector('#btn')
+btn.addEventListener('click',()=>{
+    addColor(1000,'#first','red')
+    .then(()=>addColor(4000,'#second','blue'))
+    .then(()=>addColor(2000,'#third','green'))
+    .catch((err)=>{console.log(err)})
+})
+
+function addColor(time,selector,color){
+    const element = document.querySelector(selector)
+    return new Promise((resolve,reject)=>{
+        if(element){
+            setTimeout(()=>{
+                element.style.color = color
+                resolve()
+            },time)
+        }else{
+            reject(`there is no such element: "${selector}"`)
+        }
+    })
+}
+
+//passing functions between callbacks in example above
+
+const btnData = document.querySelector('#btn')
+btnData.addEventListener('click',()=>{
+    addColorData(1000,'#first','red','hello world')
+    .then((data)=>addColorData(4000,'#second','blue',data))
+    .then((data)=>{
+        console.log(data)
+        addColorData(2000,'#third','green')
+    })
+    .catch((err)=>{console.log(err)})
+})
+
+function addColorData(time,selector,color,data){
+    const element = document.querySelector(selector)
+    return new Promise((resolve,reject)=>{
+        if(element){
+            setTimeout(()=>{
+                element.style.color = color
+                resolve(data)
+            },time)
+        }else{
+            reject(`there is no such element: "${selector}"`)
+        }
+    })
+}
